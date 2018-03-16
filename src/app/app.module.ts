f57b4a9ed97180191a044b25f7c66ece607d008f
Kumudu Gunasekara <kumudug1@gmail.com>
2018-03-17 08:16:48 +1100

    overriding child styles from parent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { BlueComponent } from './blue/blue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlueComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/blue/blue.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-blue',
+  templateUrl: './blue.component.html',
+  styleUrls: ['./blue.component.less'],
+  encapsulation: ViewEncapsulation.None
+})
+export class BlueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
